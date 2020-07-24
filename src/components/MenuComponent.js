126735fde485bbfe21d2e181c595c07301d3009c
Sumeet <itsumeet97@gmail.com>
2020-07-24 17:10:56 +0530

    week 1 assignment

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,38 +1,24 @@
 import React, {Component} from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     CardTitle } from 'reactstrap';
+import Dishdetail from './DishdetailComponent';
+
 class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      selectedDish: null
     }
   }
-  onDishSelect(dish) {
+  onSelectedDish(dish) {
         this.setState({ selectedDish: dish});
     }
 
-    renderDish(dish) {
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return(
-                <div></div>
-            );
-    }
   render() {
     const menu = this.props.dishes.map((dish) => {
              return (
-               <div  className="col-12 col-md-5 m-1">
-                 <Card key={dish.id}
-                   onClick={() => this.onDishSelect(dish)}>
+               <div key={dish.id} className="col-12 col-md-5 m-1">
+                 <Card onClick={() => this.onSelectedDish(dish)}>
                    <CardImg width="100%" src={dish.image} alt={dish.name} />
                    <CardImgOverlay>
                        <CardTitle>{dish.name}</CardTitle>
@@ -47,11 +33,7 @@ class Menu extends Component {
                  <div className="row">
                      {menu}
                  </div>
-                 <div className="row">
-                   <div  className="col-12 col-md-5 m-1">
-                     {this.renderDish(this.state.selectedDish)}
-                   </div>
-                 </div>
+                    <Dishdetail dish = {this.state.selectedDish}></Dishdetail>
              </div>
          );
      }
